c3939550bf3f6c1bc8cd47ba7bef9afbd6a6dd51
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-15 11:29:57 +0200

    Playing with Cowsay

---
+++ index.js
@@ -0,0 +1,13 @@
+let info = require("./information.js");
+let cowsay = require("cowsay");
+let message =
+  "Hello I'm" + info.name + "  " + "From: " + info.campus + " campus";
+//console.log(message);
+
+console.log(
+  cowsay.say({
+    text: message,
+    e: "oO",
+    T: "U ",
+  })
+);
---
+++ information.js
@@ -0,0 +1,5 @@
+const information = {
+  name: "David",
+  campus: "remote",
+};
+module.exports = { name: "David", campus: "Remote" };
